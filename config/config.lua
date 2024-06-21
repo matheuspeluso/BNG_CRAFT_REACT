ca1e4213bcdbadf24a1bf7b6a0207d163d15881b
Dev Matheus Peluso <matheuspeluso17@gmail.com>
2024-06-21 20:39:18 -0300

    added more new itens in config.lua and calling the same in componnent

--- config/config.lua
+++ config/config.lua
@@ -1,7 +1,7 @@
 ConfigItens = {
     WEAPON_ASSAULTRIFLE = {
         name = 'Ak-47',
-        img = '',
+        img = 'https://cdn.discordapp.com/attachments/1240421138887213108/1251315557630869524/WEAPON_ASSAULTRIFLE_MK2.png?ex=66701c2d&is=666ecaad&hm=dc2ac969b776f0b9ff65603e06cf9e22e114c13278aeeebd3bc1cb8afe252685&',
         ingredientes = {  -- tabela com os nomes do itens igual ao da base
             { item = "plastic", quantidade = 10 },
             { item = "iron", quantidade = 25 },
@@ -15,6 +15,8 @@ ConfigItens = {
 
     },
     WEAPON_PISTOL_MK2 = {
+        name = 'Pistol_mk2',
+        img = 'https://cdn.discordapp.com/attachments/1240421138887213108/1251324442194214922/WEAPON_PISTOL_MK2.png?ex=66702473&is=666ed2f3&hm=6dce9cae1068b6489f086d33b1a12d561df2f40b833f7ac656fc865289d6ca07&',
         ingredientes = {  -- tabela com os nomes do itens igual ao da base
             { item = "steel", quantidade = 20 },
             { item = "plastic", quantidade = 5 },
@@ -27,6 +29,8 @@ ConfigItens = {
         },
     },
     ammo_9 = {
+        name = 'Munição de 9mm',
+        img = 'https://cdn.discordapp.com/attachments/1240421138887213108/1251947725767970927/ammo-9.png?ex=66706ead&is=666f1d2d&hm=5485cfe6c14240a48dcb79cf5f1d411db220a2c249a0317ea61963c81c965beb&',
         ingredientes = {  -- tabela com os nomes do itens igual ao da base
             { item = "steel", quantidade = 12 },
             { item = "copper", quantidade = 6 },
@@ -37,6 +41,8 @@ ConfigItens = {
         }
     },
     ammo_rifle2 = {
+        name = 'Munição de Rifle',
+        img = 'https://cdn.discordapp.com/attachments/1240421138887213108/1252032494673330327/ammo-rifle2.png?ex=6670bda0&is=666f6c20&hm=7eee87961895b857cf205135df9972f22b7db96dd1a49e64d1137365e73292bf&',
         ingredientes = {  -- tabela com os nomes do itens igual ao da base
             { item = "steel", quantidade = 12 },
             { item = "copper", quantidade = 6 },
@@ -47,6 +53,8 @@ ConfigItens = {
         }
     },
     WEAPON_SMG = {
+        name = 'Submetralhadora',
+        img = 'https://cdn.discordapp.com/attachments/1240421138887213108/1252036208456896564/WEAPON_SMG.png?ex=6670c115&is=666f6f95&hm=4e12f752b5228df38178ac5b45fb19f00bd93f30df58a67c49b8b915bbd71569&',
         ingredientes = {  -- tabela com os nomes do itens igual ao da base
             { item = "plastic", quantidade = 10 },
             { item = "iron", quantidade = 15 },
@@ -59,6 +67,8 @@ ConfigItens = {
         }
     },
     lockpick = {
+        name = 'Lockpick',
+        img = 'https://cdn.discordapp.com/attachments/1240421138887213108/1249207739708608512/lockpick.png?ex=6671ab9e&is=66705a1e&hm=42bddde90e425234eef9a87eb95c3caacf7729db33ffbd085ee747a79f747e8f&',
         ingredientes = {  -- tabela com os nomes do itens igual ao da base
             { item = "copper", quantidade = 1 },
             { item = "steel", quantidade = 5 }
--- html/src/components/CardItem.jsx
+++ html/src/components/CardItem.jsx
@@ -16,7 +16,7 @@ const CardItem = (props) => {
     // Pegando os itens do sessionStorage
     let dados = JSON.parse(sessionStorage.getItem('itensScript'));
     const item = dados[props.nomeItem];
-
+    
     if (item) {
       return (
         <ul className={styles.ul_box}>
@@ -32,20 +32,27 @@ const CardItem = (props) => {
     }
   }
 
-  function craftar(nomeItem) {
-    enviarDadosParaoJogo('itemCraft', { item: nomeItem });
+  
+  // Acessando diretamente as propriedades do item
+  const dados = JSON.parse(sessionStorage.getItem('itensScript'));
+  const item = dados[props.nomeItem];
+  const nomeItem2 = item.name; // Acessando o nome correto do item
+  const imagem = item.img; // Acessando a imagem correta do item
+  
+  function craftar(nomeItem2) {
+    enviarDadosParaoJogo('itemCraft', { item: nomeItem2 });
   }
 
   return (
     <div className={styles.container}>
       <div className={styles.box_img}>
-        <img className={styles.img} src={props.imagem} alt={props.nomeItem} />
+        <img className={styles.img} src={imagem} alt={nomeItem2} /> {/* Utilizando a imagem correta */}
       </div>
 
       <div className={styles.box_craft}>
         <div className={styles.header}>
-          <h3>{props.nomeItem}</h3>
-          <button onClick={() => craftar(props.nomeItem)} className={styles.btn_montar}>MONTAR</button>
+          <h3>{nomeItem2}</h3> {/* Utilizando o nome correto do item */}
+          <button onClick={() => craftar(nomeItem2)} className={styles.btn_montar}>MONTAR</button>
         </div>
         <div className={styles.ingredintes}>
           {retornaLista()}
