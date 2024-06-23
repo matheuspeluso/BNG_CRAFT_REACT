c90151e5e2031405bdf6ef34159b1de98124833c
Dev Matheus Peluso <matheuspeluso17@gmail.com>
2024-06-23 17:25:36 -0300

    updated : refatored the server.lua and changed the config.lua

--- config/config.lua
+++ config/config.lua
@@ -5,6 +5,8 @@ ConfigItens = {
     craftList = {
         WEAPON_ASSAULTRIFLE = {
             name = 'Ak-47',
+            spawName = 'WEAPON_ASSAULTRIFLE',
+            spawQtd = 1,
             img = 'https://cdn.discordapp.com/attachments/1240421138887213108/1251315557630869524/WEAPON_ASSAULTRIFLE_MK2.png?ex=66701c2d&is=666ecaad&hm=dc2ac969b776f0b9ff65603e06cf9e22e114c13278aeeebd3bc1cb8afe252685&',
             ingredientes = {  -- tabela com os nomes do itens igual ao da base
                 { item = "plastic", quantidade = 10 },
@@ -18,8 +20,11 @@ ConfigItens = {
             },
     
         },
+
         WEAPON_PISTOL_MK2 = {
             name = 'Pistol_mk2',
+            spawName = 'WEAPON_PISTOL_MK2',
+            spawQtd = 1,
             img = 'https://cdn.discordapp.com/attachments/1240421138887213108/1251324442194214922/WEAPON_PISTOL_MK2.png?ex=66702473&is=666ed2f3&hm=6dce9cae1068b6489f086d33b1a12d561df2f40b833f7ac656fc865289d6ca07&',
             ingredientes = {  -- tabela com os nomes do itens igual ao da base
                 { item = "steel", quantidade = 20 },
@@ -32,8 +37,11 @@ ConfigItens = {
                 { item = "Ferro", quantidade = 15 },
             },
         },
+
         ammo_9 = {
             name = 'Munição de 9mm',
+            spawName = 'ammo-9',
+            spawQtd = 12,
             img = 'https://cdn.discordapp.com/attachments/1240421138887213108/1251947725767970927/ammo-9.png?ex=66706ead&is=666f1d2d&hm=5485cfe6c14240a48dcb79cf5f1d411db220a2c249a0317ea61963c81c965beb&',
             ingredientes = {  -- tabela com os nomes do itens igual ao da base
                 { item = "steel", quantidade = 12 },
@@ -44,8 +52,11 @@ ConfigItens = {
                 { item = "Cobre", quantidade = 6 },
             }
         },
+
         ammo_rifle2 = {
             name = 'Munição de Rifle',
+            spawName = 'ammo-rifle2',
+            spawQtd = 12,
             img = 'https://cdn.discordapp.com/attachments/1240421138887213108/1252032494673330327/ammo-rifle2.png?ex=6670bda0&is=666f6c20&hm=7eee87961895b857cf205135df9972f22b7db96dd1a49e64d1137365e73292bf&',
             ingredientes = {  -- tabela com os nomes do itens igual ao da base
                 { item = "steel", quantidade = 12 },
@@ -56,8 +67,11 @@ ConfigItens = {
                 { item = "Cobre", quantidade = 6 },
             }
         },
+
         WEAPON_SMG = {
             name = 'Submetralhadora',
+            spawName = 'WEAPON_SMG',
+            spawQtd = 1,
             img = 'https://cdn.discordapp.com/attachments/1240421138887213108/1252036208456896564/WEAPON_SMG.png?ex=6670c115&is=666f6f95&hm=4e12f752b5228df38178ac5b45fb19f00bd93f30df58a67c49b8b915bbd71569&',
             ingredientes = {  -- tabela com os nomes do itens igual ao da base
                 { item = "plastic", quantidade = 10 },
@@ -70,8 +84,11 @@ ConfigItens = {
                 { item = "Aço", quantidade = 20 }
             }
         },
+
         lockpick = {
             name = 'Lockpick',
+            spawName = 'lockpick',
+            spawQtd = 1,
             img = 'https://cdn.discordapp.com/attachments/1240421138887213108/1249207739708608512/lockpick.png?ex=6671ab9e&is=66705a1e&hm=42bddde90e425234eef9a87eb95c3caacf7729db33ffbd085ee747a79f747e8f&',
             ingredientes = {  -- tabela com os nomes do itens igual ao da base
                 { item = "copper", quantidade = 1 },
@@ -82,7 +99,26 @@ ConfigItens = {
                 { item = "Aço", quantidade = 5 }
             }
         },
-    },
+
+        AP_Pistol = {
+            name = 'AP Pistol',
+            spawName = 'WEAPON_APPISTOL',
+            spawQtd = 1,
+            img = 'https://cdn.discordapp.com/attachments/1240421138887213108/1254531389001564192/WEAPON_APPISTOL.png?ex=6679d4e7&is=66788367&hm=08b275a203fb9baab9d4fc22ddc87c0623ac6c122247318a888c93f6e91997ff&',
+            ingredientes = {  -- tabela com os nomes do itens igual ao da base
+                { item = "iron", quantidade = 15 },
+                { item = "steel", quantidade = 20 },
+                { item = "plastic", quantidade = 10 },
+            },
+            ingredientesPt = {  -- tabela com os nomes do itens traduzidos, chamados no frontend
+                { item = "Cobre", quantidade = 15 },
+                { item = "Aço", quantidade = 20 },
+                { item = "Plastico", quantidade = 10 },
+            }
+        },
+
+        
+    },--fechamento do craftList
     
 
 }
--- server/server.lua
+++ server/server.lua
@@ -10,77 +10,36 @@ RegisterNetEvent('teste:requisicao', function(cb)
     if job == permissao then
        isPolice = true
     end
-    TriggerClientEvent('teste:response', src,isPolice)
-
+    TriggerClientEvent('teste:response', src, isPolice)
 end)
 
-
-function HasAllItens(item, source)
-    -- Verifica se o item existe na tabela de itens
+function HasAllItems(item, source)
     if ConfigItens.craftList[item] then
-        -- Obtém a lista de ingredientes para o item especificado
-        local ingredientes = ConfigItens.craftList[item].ingredientes
-        
-        -- Itera sobre cada ingrediente na lista
-        for _, ingrediente in ipairs(ingredientes) do
-            -- Verifica se o jogador possui a quantidade necessária do ingrediente
-            if not exports['qb-inventory']:HasItem(source, ingrediente.item, ingrediente.quantidade) then
-                -- Se o jogador não tiver um dos ingredientes, retorna false
+        local ingredients = ConfigItens.craftList[item].ingredientes
+        for _, ingredient in ipairs(ingredients) do
+            if not exports['qb-inventory']:HasItem(source, ingredient.item, ingredient.quantidade) then
                 return false
             end
         end
-        -- Se o jogador possuir todos os ingredientes, retorna true
         return true
     else
-        -- Se o item não existir na tabela de itens, retorna false
         return false
     end
 end
 
 RegisterNetEvent('crafting')
-
 AddEventHandler('crafting', function(nomeItem)
-    
-    if HasAllItens('WEAPON_ASSAULTRIFLE',source) and nomeItem == 'WEAPON_ASSAULTRIFLE' then
-        exports.ox_inventory:RemoveItem(source, 'plastic', 10)
-        exports.ox_inventory:RemoveItem(source, 'iron',25)
-        exports.ox_inventory:RemoveItem(source, 'steel', 20)
-        TriggerClientEvent('notify_bng_success', source)
-        exports.ox_inventory:AddItem(source, 'WEAPON_ASSAULTRIFLE', 1)
-    elseif HasAllItens('WEAPON_PISTOL_MK2',source) and nomeItem == 'WEAPON_PISTOL_MK2' then
-        exports.ox_inventory:RemoveItem(source, 'plastic', 5)
-        exports.ox_inventory:RemoveItem(source, 'iron',15)
-        exports.ox_inventory:RemoveItem(source, 'steel', 20)
-        TriggerClientEvent('notify_bng_success', source)
-        exports.ox_inventory:AddItem(source, 'WEAPON_PISTOL_MK2', 1)
-    elseif HasAllItens('ammo_9',source) and nomeItem == 'ammo_9' then
-        exports.ox_inventory:RemoveItem(source, 'copper',6)
-        exports.ox_inventory:RemoveItem(source, 'steel', 12)
-        TriggerClientEvent('notify_bng_success', source)
-        exports.ox_inventory:AddItem(source, 'ammo-9', 12)
-        
-    elseif HasAllItens('ammo_rifle2',source) and nomeItem == 'ammo_rifle2' then
-        exports.ox_inventory:RemoveItem(source, 'copper',6)
-        exports.ox_inventory:RemoveItem(source, 'steel', 12)
-        TriggerClientEvent('notify_bng_success', source)
-        exports.ox_inventory:AddItem(source, 'ammo-rifle2', 12)
-    elseif HasAllItens('WEAPON_SMG',source) and nomeItem == 'WEAPON_SMG' then
-        exports.ox_inventory:RemoveItem(source, 'iron',15)
-        exports.ox_inventory:RemoveItem(source, 'steel', 20)
-        exports.ox_inventory:RemoveItem(source, 'plastic', 10)
-        TriggerClientEvent('notify_bng_success', source)
-        exports.ox_inventory:AddItem(source, 'WEAPON_SMG', 1)
-        
-    elseif HasAllItens('lockpick',source) and nomeItem == 'lockpick' then
-        print('lockpick no servidor!')
-        exports.ox_inventory:RemoveItem(source, 'copper',1)
-        exports.ox_inventory:RemoveItem(source, 'steel', 5)
-        TriggerClientEvent('notify_bng_success', source)
-        exports.ox_inventory:AddItem(source, 'lockpick', 1)
+    local playerSource = source
+    if HasAllItems(nomeItem, playerSource) then
+        local ingredients = ConfigItens.craftList[nomeItem].ingredientes
+        for _, ingredient in ipairs(ingredients) do
+            exports.ox_inventory:RemoveItem(playerSource, ingredient.item, ingredient.quantidade)
+        end
+        local spawName = ConfigItens.craftList[nomeItem].spawName
+        local spawQtd = ConfigItens.craftList[nomeItem].spawQtd
+        exports.ox_inventory:AddItem(playerSource, spawName, spawQtd)  -- Utiliza spawQtd para adicionar a quantidade correta
+        TriggerClientEvent('notify_bng_success', playerSource)
     else
-        TriggerClientEvent('notify_bng_error', source)
+        TriggerClientEvent('notify_bng_error', playerSource)
     end
-    -- exports['qb-inventory']:RemoveItem(source,'plastic',5,false,'qb-primeiroScripts:testRemove')
-    -- exports['qb-inventory']:AddItem(source,'lockpick',1,false,'qb-primeiroScripts:testAdd')
 end)
-
